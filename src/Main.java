95d73dc388898118cb9d1be5fbccce268153a2ac
MrTOOGLE <Leva.mokrov@gmail.com>
2024-07-12 23:45:51 +0800

    working prototype
    make a code

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,24 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Parameters parameters = analyzeParameters(args);
+        Parameters parameters = parametersAnalyzer(args);
         BufferedReader reader;
+        String resInt = "";
+        String resFloat = "";
+        String resString = "";
         for (File file : parameters.inputFiles()) {
             try {
                 reader = new BufferedReader(new FileReader(file));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    String analysis = lineAnalyzer(line);
+                    switch (analysis) {
+                        case "str" -> resString += line + "\n";
+                        case "float" -> resFloat += line + "\n";
+                        case "int" -> resInt += line + "\n";
+                    }
                 }
+                //TODO: добавить статистику + "-a"
             } catch (FileNotFoundException e) {
                 System.out.printf("Файла %s не существует", file);
             }
@@ -18,9 +27,12 @@ public class Main {
                 System.out.println("Ошибка ввода/вывода");
             }
         }
+        write(resString, "strings", parameters);
+        write(resFloat, "floats", parameters);
+        write(resInt, "integers", parameters);
     }
 
-    private static Parameters analyzeParameters(String[] args) {
+    private static Parameters parametersAnalyzer(String[] args) {
         Parameters.BuildParameters buildParameters = new Parameters.BuildParameters();
         boolean isPath = false;
         boolean isPrefix = false;
@@ -48,5 +60,48 @@ public class Main {
             }
         }
         return buildParameters.build();
+    }
+
+    private static String lineAnalyzer(String line) {
+        char[] chars = line.toCharArray();
+        boolean isStr = false, isFloat = false;
+        int eCounter = 0;
+        for (char c : chars) {
+            // на всякий случай проверка на русскую е
+            if (Character.isLetter(c) && c != 'E' && c != 'Е') {
+                isStr = true;
+                return "str";
+            }
+            else if (c == 'E' || c == 'Е') {
+                if (eCounter > 0) {
+                    return "str";
+                }
+                else {
+                    eCounter++;
+                }
+            }
+            else if (c == '.') {
+                return "float";
+            }
+        }
+        return "int";
+    }
+
+    private static void write(String result, String name, Parameters parameters) {
+        try {
+            if (!result.equals("")) {
+                String path = parameters.path() + "\\" + parameters.prefix() + name + ".txt";
+                System.out.println(path);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+                for (String s : result.split("\n")) {
+                    writer.write(s);
+                    System.out.println(s);
+                }
+            }
+            System.out.println("------------------------------------------------");
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка ввода/вывода");
+        }
     }
 }
