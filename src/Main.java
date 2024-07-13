444df697f419860868fd4d27727127c1163c3b52
MrTOOGLE <Leva.mokrov@gmail.com>
2024-07-13 15:38:50 +0800

    some changes
    aboba

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         String resInt = "";
         String resFloat = "";
         String resString = "";
-        for (File file : parameters.inputFiles()) {
+        for (String filePath : parameters.inputFiles()) {
             try {
-                reader = new BufferedReader(new FileReader(file));
+                reader = new BufferedReader(new FileReader(filePath));
                 String line;
                 while ((line = reader.readLine()) != null) {
                     String analysis = lineAnalyzer(line);
@@ -19,17 +19,16 @@ public class Main {
                         case "int" -> resInt += line + "\n";
                     }
                 }
-                //TODO: добавить статистику + "-a"
+                reader.close();
             } catch (FileNotFoundException e) {
-                System.out.printf("Файла %s не существует", file);
-            }
-            catch (IOException e) {
+                System.out.printf("Файла %s не существует", filePath);
+            } catch (IOException e) {
                 System.out.println("Ошибка ввода/вывода");
             }
         }
-        write(resString, "strings", parameters);
-        write(resFloat, "floats", parameters);
-        write(resInt, "integers", parameters);
+        write(resString, "strings", parameters, "String");
+        write(resFloat, "floats", parameters, "Float");
+        write(resInt, "integers", parameters, "Integer");
     }
 
     private static Parameters parametersAnalyzer(String[] args) {
@@ -42,20 +41,18 @@ public class Main {
                     case "-o" -> isPath = true;
                     case "-p" -> isPrefix = true;
                     case "-a" -> buildParameters.setRewrite(false);
-                    case "-s" -> buildParameters.setInfo(false);
+                    case "-s" -> buildParameters.setInfo("short");
+                    case "-f" -> buildParameters.setInfo("full");
                 }
-            }
-            else {
+            } else {
                 if (isPath) {
                     buildParameters.setPath(arg);
                     isPath = false;
-                }
-                else if (isPrefix) {
+                } else if (isPrefix) {
                     buildParameters.setPrefix(arg);
                     isPrefix = false;
-                }
-                else {
-                    buildParameters.addInputFile(new File(arg));
+                } else {
+                    buildParameters.addInputFile(arg);
                 }
             }
         }
@@ -64,44 +61,89 @@ public class Main {
 
     private static String lineAnalyzer(String line) {
         char[] chars = line.toCharArray();
-        boolean isStr = false, isFloat = false;
         int eCounter = 0;
         for (char c : chars) {
             // на всякий случай проверка на русскую е
             if (Character.isLetter(c) && c != 'E' && c != 'Е') {
-                isStr = true;
                 return "str";
-            }
-            else if (c == 'E' || c == 'Е') {
+            } else if (c == 'E' || c == 'Е') {
                 if (eCounter > 0) {
                     return "str";
-                }
-                else {
+                } else {
                     eCounter++;
                 }
-            }
-            else if (c == '.') {
+            } else if (c == '.') {
                 return "float";
             }
         }
         return "int";
     }
 
-    private static void write(String result, String name, Parameters parameters) {
-        try {
-            if (!result.equals("")) {
-                String path = parameters.path() + "\\" + parameters.prefix() + name + ".txt";
-                System.out.println(path);
-                BufferedWriter writer = new BufferedWriter(new FileWriter(path));
-                for (String s : result.split("\n")) {
-                    writer.write(s);
-                    System.out.println(s);
+    private static String getStatistics(String str, String type, String typeStat) {
+        int length = str.split("\n").length;
+        String result = String.format("Кол-во элементов у типа %s - %d\n", type, length);
+        switch (typeStat) {
+            case "integer" -> {
+                long max = 0;
+                // TODO: понять почему тут ошибка, сделать отдельный класс для анализа и статистики
+                long min = 9999999999999;
+                long sum = 0;
+                for (String s : str.split("\n")) {
+                    long num = 0;
+                    try {
+                        num = Long.parseLong(s);
+                    } catch (NumberFormatException ignored) {
+                    }
+                    sum += num;
+                    max = Math.max(max, num);
+                    min = Math.min(min, num);
                 }
+                result += String.format("Максимальное число = %d\nМинимальное число = %d\nСумма элементов = %d\nСреднее значение = %f\n", max, min, sum, (float) sum / length);
             }
-            System.out.println("------------------------------------------------");
+            case "float" -> {
+                float max = 0;
+                float min = 999999999;
+                float sum = 0;
+                for (String s : str.split("\n")) {
+                    float num = 0;
+                    try {
+                        num = Float.parseFloat(s);
+                    } catch (NumberFormatException ignored) {
+                    }
+                    sum += num;
+                    max = Math.max(max, num);
+                    min = Math.min(min, num);
+                }
+                result += String.format("Максимальное число = %f\nМинимальное число = %f\nСумма элементов = %f\nСреднее значение = %f\n", max, min, sum, sum / length);
+            }
+            case "string" -> {
+                int max = 0;
+                int min = 999999999;
+                for (String s : str.split("\n")) {
+                    max = Math.max(max, s.length());
+                    min = Math.min(min, s.length());
+                }
+                result += String.format("Максимальная длина строки = %d\nМинимальная длина строки = %d\n", max, min);
+            }
         }
-        catch (IOException e) {
-            System.out.println("Ошибка ввода/вывода");
+        return result;
+    }
+
+    private static void write(String result, String name, Parameters parameters, String type) {
+        if (!result.equals("")) {
+            try {
+                String path = parameters.path() + "\\" + parameters.prefix() + name + ".txt";
+                // !parameters.rewrite() - для добавления/перезаписывания файла
+                BufferedWriter writer = new BufferedWriter(new FileWriter(path, !parameters.rewrite()));
+                writer.write(result);
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка ввода/вывода");
+            }
+            if (!parameters.info().equals("")) {
+                String message = getStatistics(result, type, parameters.info().equals("full") ? type.toLowerCase() : "");
+                System.out.println(message);
+            }
         }
     }
 }
--- src/Parameters.java
+++ src/Parameters.java
@@ -1,17 +1,16 @@
-import java.io.File;
 import java.util.ArrayList;
 
 /*
 Класс для обработки аргументов, полученных при запуске jar
 Идея для данного вида обработки была взята из лабы по java за третий семак
 */
-public record Parameters(String path, String prefix, boolean rewrite, boolean info, ArrayList<File> inputFiles) {
+public record Parameters(String path, String prefix, boolean rewrite, String info, ArrayList<String> inputFiles) {
     public static class BuildParameters {
         private String path = ""; // По умолчанию кидаем в ту же папку
         private String prefix = ""; // По умолчанию префикса нема
         private boolean rewrite = true; // По умолчанию файлы перезаписываем
-        private boolean info = true; // -s (short=false) / -f (fool=true)
-        private final ArrayList<File> inputFiles = new ArrayList<>(); // Файлы для чтения
+        private String info = ""; // -s (short) / -f (full) / "" (none)
+        private final ArrayList<String> inputFiles = new ArrayList<>(); // Файлы для чтения
 
         public void setPath(String path) {
             this.path = path;
@@ -25,11 +24,11 @@ public record Parameters(String path, String prefix, boolean rewrite, boolean in
             this.rewrite = rewrite;
         }
 
-        public void setInfo(boolean info) {
+        public void setInfo(String info) {
             this.info = info;
         }
 
-        public void addInputFile(File inputFile) {
+        public void addInputFile(String inputFile) {
             inputFiles.add(inputFile);
         }
 
